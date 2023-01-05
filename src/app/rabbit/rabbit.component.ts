679b4eeed43ab4508750315406678637e6a2ff03
Gabor Kiacz <info@lilagoz.hu>
2023-01-05 13:40:41 +0100

    custom validator

--- src/app/rabbit/rabbit.component.ts
+++ src/app/rabbit/rabbit.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormBuilder,
+  ValidationErrors,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-rabbit',
@@ -10,11 +15,16 @@ export class RabbitComponent implements OnInit {
   constructor(private fb: FormBuilder) {}
   ngOnInit() {}
 
+  onlyTheGoat(control: AbstractControl): ValidationErrors | null {
+    console.log('onlyTheGoat', control);
+    return control.value === 'goat' ? null : { onlyTheGoat: true };
+  }
+
   // this form group was created by form builder
   rabbitForm = this.fb.group({
     foo: '',
     bar: [''], // there is no required validator BUT
-    baz: ['', [Validators.required]], // there is required
+    baz: ['', [Validators.required, this.onlyTheGoat]], // there is required
   });
   rabbitFormSubmitHandle() {
     console.warn(this.rabbitForm.errors);
