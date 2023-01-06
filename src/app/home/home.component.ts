5011ba51a0a665f83cee5c7bbc28ad89fb5c990f
Gabor Kiacz <info@lilagoz.hu>
2023-01-06 12:12:11 +0100

    wip

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, of } from 'rxjs';
-import { FakeDataService } from '../fake-data.service';
-import { FormControl, FormGroup } from '@angular/forms';
+
+interface Animal {
+  feed(): void;
+}
+class Puppy implements Animal {
+  isAnimal() {
+    return 'feed' in this;
+  }
+  feed(): void {
+    console.log('omnomnom');
+  }
+}
+
+export interface MyInterface {
+  getName(): string | null;
+}
+
+export function isMyInterface(object: any): object is MyInterface {
+  const myInterface = object as MyInterface;
+  return myInterface.getName !== undefined;
+}
 
 @Component({
   selector: 'app-home',
@@ -9,8 +27,12 @@ import { FormControl, FormGroup } from '@angular/forms';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  data$: Observable<any> | undefined;
+  buksi = new Puppy();
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    if (this.buksi.isAnimal()) {
+      this.buksi.feed();
+    } else console.error(`wau`);
+  }
 }
